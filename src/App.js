66a1e005abb1745dc31fb5ec734c65560716dcf2
OmarKhatib01 <osokhatib1@gmail.com>
2021-10-24 14:00:20 -0500

    filter

--- src/App.js
+++ src/App.js
@@ -21,19 +21,37 @@ const leave = (player_count, event_id,user,userEvents) => {
 }
 
 
+const filterEvents = (events, sport, court, date) => {
+  let filteredEvents = events
+  if (sport) {
+    filteredEvents = filteredEvents.filter((event) => event.sport_id === sport)
+  }
+  if (court) {
+    filteredEvents = filteredEvents.filter((event) => event.court_id === court)
+  }
+  if (date) {
+    const filterDate = (new Date(date*1000)).toLocaleDateString()
+    filteredEvents = filteredEvents.filter((event) => (new Date(event.date*1000)).toLocaleDateString() === filterDate)
+  }
+  return filteredEvents.sort((e1, e2) => e1.date - e2.date)
+}
 // const SportButton = ({sport, selected})
 
 
 
 function App() {
   const [data, loading, error] = useData("/")
-  const [user,setUser] = useState("idU0")
+  const [user, setUser] = useState("idU0")
+  const [sport, setSport] = useState('')
+  const [court, setCourt] = useState('')
+  const [date, setDate] = useState('')
+
   if (error) return <h1>{error}</h1>
   if (loading) return <h1>loading...</h1>
   return (
     <div>
       <h1>PickMeUp</h1>
-      {Object.values(data.events).map(event => (
+      {filterEvents(Object.values(data.events), sport, court, date).map(event => (
        <div className = 'col-lg-8 card m-2 p-2 mx-auto border-dark'> 
           <div className = 'card-body d-flex align-items-center justify-content-between'>
           <div>
