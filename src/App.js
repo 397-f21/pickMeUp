d75b86828b0b29a60335d329f7f3097da4eec3f2
Caspar Popova <71401509+cjpopova@users.noreply.github.com>
2021-10-26 22:12:55 -0500

    Modularize code to EventCard and EventList

--- src/App.js
+++ src/App.js
@@ -1,42 +1,7 @@
 import { updateDataByPath, useData, } from "./firebase";
 import {useState} from 'react'
 import EventFilterModal from "./components/EventFilterModal";
-import { FcSportsMode } from "react-icons/fc";
-
-
-const join = (player_count, event_id,user,userEvents) => {
-  if (!userEvents){
-    userEvents = []
-  }
-  updateDataByPath(`/users/${user}/`, {events: [...userEvents, event_id]})
-  updateDataByPath(`/events/${event_id}/`, {player_count:player_count+1})
-    
-}
-
-const leave = (player_count, event_id,user,userEvents) => {
-  if (!userEvents){
-    userEvents = []
-  }
-  updateDataByPath(`/users/${user}/`, {events: userEvents.filter(event => event !== event_id)})
-  updateDataByPath(`/events/${event_id}/`, {player_count: (player_count > 0) ? player_count-1 : 0})
-  
-}
-
-
-const filterEvents = (events, sport, court, date) => {
-  let filteredEvents = events
-  if (sport) {
-    filteredEvents = filteredEvents.filter((event) => event.sport_id === sport)
-  }
-  if (court) {
-    filteredEvents = filteredEvents.filter((event) => event.court_id === court)
-  }
-  if (date) {
-    const filterDate = (new Date(date*1000)).toLocaleDateString()
-    filteredEvents = filteredEvents.filter((event) => (new Date(event.date*1000)).toLocaleDateString() === filterDate)
-  }
-  return filteredEvents.sort((e1, e2) => e1.date - e2.date)
-}
+import EventList from "./components/EventList";
 
 function App() {
   const [data, loading, error] = useData("/")
@@ -60,7 +25,6 @@ function App() {
           {data.sports[sport].name} Events
         </h4>
       )}
-      
 
       <div className="text-center">
       <button className="btn btn-primary" onClick={handleShowFilter}>
@@ -80,40 +44,7 @@ function App() {
             setDate={setDate} />
       )}
 
-
-
-      {filterEvents(Object.values(data.events), sport, court, date).map(event => (
-       <div className = 'col-lg-8 card m-2 p-1 mx-auto border-dark' key = {event.event_id}> 
-          <div className = 'card-body d-flex align-items-center justify-content-between'>
-          <div>
-              <div className="fw-bold">
-                { data.sports[event.sport_id].name }
-              </div>
-              <div>
-                { data.sports[event.sport_id].courts[event.court_id].name }
-              </div>
-              <div className="card-text">
-                { (new Date(event.date*1000)).toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'}) }
-              </div>
-          </div>
-          <div className="d-flex flex-nowrap flex-column align-items-center">
-            <div className="mb-1">
-              { event.player_count }
-              <FcSportsMode style={{height: '1.5em', width: '1.5em'}}/>
-            </div>
-            {( data.users[user].events && data.users[user].events.includes(event.event_id)) ? (
-              <button className='btn btn-warning bg-opacity-25' style={{width:"6em"}} onClick={ (ev) => leave(event.player_count, event.event_id, user, data.users[user].events) } >Leave</button>
-            ) :
-            (
-              <button className='btn btn-success bg-opacity-25' style={{width:"6em"}} onClick={ (ev) => join(event.player_count, event.event_id, user, data.users[user].events) } >Join</button>
-            )
-          
-          }
-    
-          </div>
-         </div>
-       </div> 
-      ))}
+      <EventList data={data} sport={sport} court={court} date={date} user={user} />
     </div>
   );
 }
---
+++ src/components/EventList.js
@@ -0,0 +1,23 @@
+import EventCard from "./EventCard"
+
+const filterEvents = (events, sport, court, date) => {
+    let filteredEvents = events
+    if (sport) {
+      filteredEvents = filteredEvents.filter((event) => event.sport_id === sport)
+    }
+    if (court) {
+      filteredEvents = filteredEvents.filter((event) => event.court_id === court)
+    }
+    if (date) {
+      const filterDate = (new Date(date*1000)).toLocaleDateString()
+      filteredEvents = filteredEvents.filter((event) => (new Date(event.date*1000)).toLocaleDateString() === filterDate)
+    }
+    return filteredEvents.sort((e1, e2) => e1.date - e2.date)
+  }
+
+const EventList = ({data, sport, court, date, user}) => (
+filterEvents(Object.values(data.events), sport, court, date).map(event => 
+    <EventCard data={data} event={event} user={user} />
+  ))
+
+export default EventList
