b99918fcb341ebfe6eb81adc59b29679b3df1c4e
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-10-29 22:00:23 -0500

    Fixed typo in test message

--- src/components/EventList.test.js
+++ src/components/EventList.test.js
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import EventList from './EventList';
 import data from '../../docs/schema.json';
 
-test('show only tennis events when filterd by tennis', () => {
+test('show only tennis events when filtering by tennis', () => {
   const eventlist = render(
     <EventList 
       data={data} 
