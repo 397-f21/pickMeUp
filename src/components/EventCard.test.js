fafcac62091f320e3e9513c27f131770bd196096
Jun-Li8 <zeli2022@u.northwestern.edu>
2021-11-01 19:25:00 -0500

    created jun test

--- src/components/EventCard.test.js
+++ src/components/EventCard.test.js
@@ -1,4 +1,10 @@
 
 import { render } from '@testing-library/react';
-import EventList from './EventList';
 import data from '../../docs/schema.json';
+import EventCard from './EventCard'
+
+test("If user hasn't already joined event, button toggles to join action", () => {
+    const EventCard = render(
+
+    );
+})
