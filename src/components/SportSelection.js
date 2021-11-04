c252929a5cdc6d64d0f761ae9488fc21bb94b50f
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-11-03 21:26:14 -0500

    Deployed to firebase

--- src/components/SportSelection.js
+++ src/components/SportSelection.js
@@ -6,31 +6,35 @@ import {GiFrisbee}from "react-icons/gi"
 
 
 const icons = {
-    idS0:<GiTennisRacket className = "card-img-top" style = {{height:"5em"}}/>,
-    idS1:<GiBasketballBasket className = "card-img-top" style = {{height:"5em"}}/>,
-    idS2:<BiFootball className = "card-img-top"style = {{height:"5em"}}/>,
-    idS3:<GiVolleyballBall className = "card-img-top"style = {{height:"5em"}}/> ,
-    idS4:<GiFrisbee className = "card-img-top"style = {{height:"5em"}}/>
+  idS0:<GiTennisRacket className = "card-img-top" style = {{height:"5em"}}/>,
+  idS1:<GiBasketballBasket className = "card-img-top" style = {{height:"5em"}}/>,
+  idS2:<BiFootball className = "card-img-top"style = {{height:"5em"}}/>,
+  idS3:<GiVolleyballBall className = "card-img-top"style = {{height:"5em"}}/> ,
+  idS4:<GiFrisbee className = "card-img-top"style = {{height:"5em"}}/>
 }
 
 const SportSelection =({sports,setSport}) => (
-        <>
-        <h1 className = 'text-center mb-5'> Choose your sports!</h1>
-        <div className = 'container-sm d-flex flex-wrap justify-content-center' style = {{maxWidth:'800px'}}>
-            {
-                Object.values(sports).map(sport => (
-                    
-                    <div className="card p-2 m-2 col-5 rounded" style = {{borderWidth:'5px', borderRadius:'10%'}}>
-                        {icons[sport.sport_id]}
-                        <div className="card-body">
-                            <a onClick={ () => setSport(sport.sport_id)} href="#" className="stretched-link btn btn-primary w-100">{sport.name} </a>
-                        </div>
-                    </div>
-    
-                ))
-            }
-        </div>
-        </>
-    )
+  <>
+    <h1 className='text-center mb-5'>Choose your sports!</h1>
+    <div className='container-sm d-flex flex-wrap justify-content-center' style = {{maxWidth:'800px'}}>
+      {
+        Object.values(sports).map(sport => (
+          <div key={sport.sport_id}
+              className="card p-2 m-2 col-5 rounded" 
+              style={{borderWidth:'5px'}}>
+            {icons[sport.sport_id]}
+            <div className="card-body">
+              <a  href="#"
+                  onClick={ () => setSport(sport.sport_id)} 
+                  className="stretched-link btn btn-primary w-100">
+                {sport.name} 
+              </a>
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  </>
+);
 
 export default SportSelection
