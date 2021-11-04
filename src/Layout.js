e9ac0acd7cd5060055bb27d9c26f02b6e58a9cc2
Jun-Li8 <zeli2022@u.northwestern.edu>
2021-11-03 20:02:59 -0500

    render capacity

--- src/Layout.js
+++ src/Layout.js
@@ -21,12 +21,12 @@ const Layout = ({ children }) => {
   <div>
     <div className='navbar navbar-light bg-warning bg-light'>
       <div className='container-fluid'>
-        <span className='navbar-brand justify-content-bottom'>
+        <a className='navbar-brand justify-content-bottom' href="/">
           <img src={ logo } alt="PickMeUp Logo" width="30" height="30" style={{marginTop: '-.4em', marginRight: '.2em'}}/>
           <span className="h2 fw-bold"> 
             PickMeUp 
           </span>
-        </span>
+        </a>
         
         {user ? <SignOutButton /> : <SignInButton />}
       </div>
