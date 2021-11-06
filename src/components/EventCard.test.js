8704e7b8eefb8a04e19af1ba823f54d1181a954b
Caspar Popova <71401509+cjpopova@users.noreply.github.com>
2021-11-06 12:55:40 -0500

    Complete player count incrementation test

--- src/components/EventCard.test.js
+++ src/components/EventCard.test.js
@@ -1,18 +1,22 @@
 import { render, screen, act } from '@testing-library/react';
 import EventCard from './EventCard.js';
 import data from '../../docs/schema.json';
-import { updateDataByPath } from '../firebase.js';
-
-jest.mock("../firebase.js", () => {
-  return function DummyUpdateDataByPath(path, value) { return "success" }
-});
 
 test('given a user is logged in, increment player count on join', () => {
+  // mock firebase's updateDataByPath ~ first call updates the user list; second call updates the player_count
+  const spyUpateDataByPath = jest
+  .spyOn(require('../firebase.js'), 'updateDataByPath')
+  .mockImplementationOnce( (path, value) => "success")
+  .mockImplementationOnce((path, value) => {
+            data.events.idE4.player_count = data.events.idE4.player_count + 1;
+            return "success";
+          });
+
   const evnt = data.events.idE4;
   const usr = data.users.idU0;
   const eventCard = render(
     <EventCard data={data} event={evnt} user={usr}/>
-  ) 
+  );
   const nameElement = screen.getByTestId('event-card-player-count');
 
   // check that the text content matches the current player count
@@ -21,11 +25,9 @@ test('given a user is logged in, increment player count on join', () => {
   
   // fire the button
   const btn = document.querySelector("[data-testid=join-btn]");
-  // TODO: need to mock the button's onchange to prevent it from talking to firebase
   act(() => {
     btn.dispatchEvent(new MouseEvent("click", { bubbles: true }));
   });
-  
 
   // check that the text content is updated
   const rg2 = new RegExp(`${evnt.player_count+1}`);
